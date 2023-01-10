95820be11248383e2b5b1906e73fbf0ae143d94d
Toshifumi Kurosawa <Kuromailserver@gmail.com>
2023-01-10 15:01:15 +0900

    s youtube api

--- app.ts
+++ app.ts
@@ -1,5 +1,24 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+const API_KEY = 'AIzaSyBamG77fPrxng8AGGXT5syA6so2bcvt8xg'
+const url = `https://www.googleapis.com/youtube/v3/search?key=$${API_KEY}`;
+
+const options: AxiosRequestConfig = {
+    url: `${url}`,
+    method: "GET",
+};
+
 async function app() {
     console.log("start app")
+    await axios(options)
+        .then((res: AxiosResponse<any>) => {
+            const { data, status } = res;
+            //やりたいことをやる
+            console.log({ data })
+        })
+        .catch((e: AxiosError<{ error: string }>) => {
+            // エラー処理
+            console.log(e.message);
+        });
     console.log("end app")
 }
 
